1ad4b95df0c4b17f3c4147e544bb92bf1df40c21
shiv2804sr-cyber <shiv2804sr@gmail.com>
2026-07-18 20:37:03 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+ const accountId = 144553
+ let accountEmail = "shivam@google.com"
+ var accountPassword = "1234"
+accountCity = "jaipur"
+let accountState;
+// accountId =7; //not allowed
+
+accountEmail ="shiv@123";
+accountPassword = "212121"
+accountCity ="bengaluru";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+ "use strict"; //treat all js code as newer version 
+ //alert( 3+3)// we are using nodejs, not browser
+
+ console.log(3+3);
+
+ console.log("shivam");
+ //code readability should be high//
+
+ let name = "shivam";
+ let age = 23;
+ let isLoggedIn = false;
+ let state;
+ //number => 2 to power 53
+ //bigint
+ //string => ""
+ //boolean => true or false
+ //null => standalone value(empty)
+ //undefined => not assigned
+ //symbol => used for unique value
+
+ //object 
+ console.log(typeof "shivam" );
+ console.log(typeof age);
+ console.log(typeof null);//object output
+ console.log(typeof undefined);//undefined output
+ 
+ 
+ 
+ 
