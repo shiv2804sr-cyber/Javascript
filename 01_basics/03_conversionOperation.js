5ed10c1660589508b248ada9fd219a093c3de871
shiv2804sr-cyber <shiv2804sr@gmail.com>
2026-07-19 18:10:46 +0000

    conversion change done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,50 @@
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// **************** Operations *******************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);(addition)
+// console.log(2-2);(subtraction)
+// console.log(2*2);(multiply)
+// console.log(2/2);(divide)
+// console.log(2**3);(power of)
+// console.log(2%3);(modulus)
+
+let str1 = "hello"
+let str2 = " shivam"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(3+4)*5%3);
+
+// console.log(true);//ye hume true output dega 
+// console.log(+true);//ye 1 output dega
+// console.log(+"");//ye 1 0 dega
+//console.log(true +);//ye nhi krna chaiye
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2// sahi h par readable nhi h ye nhi krna chaiye
+
+let  gameCounter = 100
+gameCounter++;//output 101
+++gameCounter;//output 101
+//par ek prefix operator h ek postfix operator 
+console.log(gameCounter);
+
+
+
