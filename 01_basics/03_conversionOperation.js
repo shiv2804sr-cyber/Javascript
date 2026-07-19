c824292753b30d7bb804b05422dc6715f84bfe59
shiv2804sr-cyber <shiv2804sr@gmail.com>
2026-07-19 16:39:55 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+
+  let score = "33abc"
+
+ //const {score} = req.body//
+
+// console.log(typeof score);
+ //console.log(typeof (score));
+ 
+ let ValueInNumber =Number(score)
+ //console.log(typeof ValueInNumber);
+ //console.log(ValueInNumber);
+
+ //"33" => 33 mein convert ho jata h
+ // "33abc" => Nan(not a number)
+ //true =>1; false=>0;
+
+
+ let isLoggedIn = 1
+
+ let  booleanisLoggedIn =  Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+//1 => true; 0 => false;
+//"" =>false
+//"shivam" =>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
