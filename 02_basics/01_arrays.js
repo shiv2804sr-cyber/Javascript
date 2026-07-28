7d140dd11548fe1b6f625a334aab81eb5305c3c5
shiv2804sr-cyber <shiv2804sr@gmail.com>
2026-07-28 15:36:08 +0000

    done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,7 +1,7 @@
- // array
+//  array
 
 const myArr = [0, 1, 2, 3, 4, 5]
-// console.log(myArr[0]);
+console.log(myArr[0]);
 
 const myHeors = ["shaktiman", "naagraj"]
 
@@ -10,14 +10,14 @@ console.log(myArr[1]);
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
-// myArr.unshift(9)
-// myArr.shift()
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+myArr.unshift(9)
+myArr.shift()
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
 
 const newArr = myArr.join()
 
@@ -25,16 +25,16 @@ console.log(myArr);
 console.log( newArr);
 
 
-// // slice, splice
+// slice, splice
 
-// console.log("A ", myArr);
+console.log("A ", myArr);
 
-// const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3)
 
-// console.log(myn1);
-// console.log("B ", myArr);
+console.log(myn1);
+console.log("B ", myArr);
 
 
-// const myn2 = myArr.splice(1, 3)
-// console.log("C ", myArr);
-// console.log(myn2);
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
